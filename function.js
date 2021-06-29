764f9f294af530abd088acabb67a2e6ca2257c7f
Rizkia-AS <rizkia.as.ac@gmail.com>
2021-06-29 10:14:18 +0900

    save function.js

---
+++ function.js
@@ -0,0 +1,22 @@
+// FUNCTION MODULE
+
+
+function upData(key,data) {
+	localStorage.setItem(key, JSON.stringify(data));
+}
+
+function loadData(key) {
+	return JSON.parse(localStorage.getItem(key));
+}
+
+function changeTheme(target) {
+	if(target == 'Neon') {
+		link.href = 'assets/css/neonUI.css';
+		background.style.display = 'block';
+	}
+
+	if(target == 'Dark') {
+		link.href = 'assets/css/darkUI.css';
+		background.style.display = 'none';
+	}
+}
